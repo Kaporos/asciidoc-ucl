13bd12bd4bbd391ede4a9008f6940e639d5a46a6
Kaporos <theo@daron.be>
2022-10-09 21:17:04 +0200

    refactor: ♻️ Placing theme inside a theme directory

--- build_pdf.sh
+++ build_pdf.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p build
 echo "[*] Building pdf..."
-asciidoctor-pdf -a pdf-fontsdir="GEM_FONTS_DIR,fonts" -a pdf-theme="pdf.yml"  ${1:-README.adoc}  -o build/${1:-output}.pdf
+asciidoctor-pdf -a pdf-fontsdir="GEM_FONTS_DIR,theme/fonts" -a pdf-theme="theme/pdf.yml"  ${1:-README.adoc}  -o build/${1:-output}.pdf
 echo "[*] Done ! Check it out in ./build/${1:-output}.pdf"
