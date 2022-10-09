6bbe861663a2e2468dcda3e1b09a167a9f1226ce
Kaporos <theo@daron.be>
2022-10-09 22:30:28 +0200

    refactor: ♻️ Using docker and adding maths support

--- build_pdf.sh
+++ build_pdf.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 mkdir -p build
 echo "[*] Building pdf..."
-asciidoctor-pdf -a pdf-fontsdir="GEM_FONTS_DIR,theme/fonts" -a pdf-theme="theme/pdf.yml"  ${1:-README.adoc}  -o build/${1:-output}.pdf
+echo "[*] If there is maths inside your documents, a stem directory will be created. Don't delete it."
+docker run -it --rm -v $PWD:/documents/ asciidoctor/docker-asciidoctor \
+asciidoctor-pdf -a pdf-fontsdir="GEM_FONTS_DIR,theme/fonts" -a pdf-theme="theme/pdf.yml" -r asciidoctor-mathematical ${1:-README.adoc} -o build/${1:-output}.pdf
+
 echo "[*] Done ! Check it out in ./build/${1:-output}.pdf"
